1e7105f3cfaea5743f6005fcc9e573f1d7b6e2cb
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-30 20:54:27 +0200

    added section3 for home page

---
+++ frontend/src/pages/home/components/section3.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+// import { Link } from 'react-router-dom';
+import "./section3.css";
+
+const Section3 = () => {
+  return (
+    <div>
+      <div className="section3">
+        <div className="section3-container1">
+          <div className="section3-container1-1">
+            <h4>Buy Your Ticket</h4>
+            <div className="container1-1-1">
+              <div className="section3-from">
+                <img src="/imgs/location-icon.png" alt="" />
+                <select name="" id="" className="section3-select">
+                  <option value="Beirut">Beirut</option>
+                </select>
+              </div>
+              <div className="section3-to">
+                <img src="/imgs/location-icon.png" alt="" />
+                <select name="" id="" className="section3-select">
+                  <option value="Saida">Saida</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="section3-container1-2">
+            <div className="section3-depart">
+              <img src="/imgs/calendar-icon.png" alt="" />
+              <select name="" id="" className="section3-select">
+                <option value="Depart">Depart</option>
+              </select>
+            </div>
+            <div className="section3-arrive">
+              <img src="/imgs/calendar-icon.png" alt="" />
+              <select name="" id="" className="section3-select">
+                <option value="Arrive">Arrive</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="section3-container2">
+          <img src="/imgs/search-icon2.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -3,16 +3,18 @@ import React from "react";
 import Hero from "./components/hero";
 import Section1 from "./components/section1";
 import Section2 from "./components/section2";
+import Section3 from "./components/section3";
 
 const HomePage = () => {
   return (
     <div>
         <Hero/>
         <Section1/> 
-        <Section2/>     
-      <footer>
+        <Section2/>    
+        <Section3/> 
+      {/* <footer>
         <p>&copy; 2024 Subway System. All rights reserved.</p>
-      </footer>
+      </footer> */}
     </div>
   );
 };
