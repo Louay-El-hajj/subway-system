bed96ea934cd852c0d39087bfd16517675459932
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-30 18:15:19 +0200

    added section2 of the home page

---
+++ frontend/src/pages/home/components/section2.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import { Link } from 'react-router-dom';
+import "./section2.css";
+
+const Section2 = () => {
+  return (
+    <div>
+      <div className="section2-title">
+        <h3>Hot Destinations</h3>
+      </div>
+      <section className="section2-container">
+        <div>
+          <img src="/imgs/img-beirut.png" alt="" />
+        </div>
+        <div className="section2-container-text">
+        <p>From:</p>
+        <p>To:</p>
+        <p>Price:</p>
+        <p>Rating:</p>
+        </div>
+        <button className="section2-button">Book</button>
+      </section>
+    </div>
+  );
+};
+
+export default Section2;
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 // import { Link } from 'react-router-dom';
 import Hero from "./components/hero";
 import Section1 from "./components/section1";
+import Section2 from "./components/section2";
 
 const HomePage = () => {
   return (
     <div>
         <Hero/>
-        <Section1/>      
+        <Section1/> 
+        <Section2/>     
       <footer>
         <p>&copy; 2024 Subway System. All rights reserved.</p>
       </footer>
