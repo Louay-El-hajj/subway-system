a2c2d2e1fa922594b41e0c23e948ff124d0bc1e5
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-31 20:50:58 +0300

    fixed bugs

--- frontend/src/components/landingScreen/footer.jsx
+++ frontend/src/components/landingScreen/footer.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "../../styles/landingScreen/footer.css";
+import "../../styles/commons/utilities.css";
+import "../../styles/commons/colors.css";
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-section1">
+    <footer className="footer secondary-bg">
+      <div className="footer-section1 flex">
         <div className="footer-section1-img">
         <img src="/imgs/logo-footer.png" alt="" />
         </div>
-        <div className="footer-info">
+        <div className="footer-info flex">
           <p>Destinations</p>
           <p>About</p>
           <p>Contact</p>
         </div>
       </div>
-      <div className="footer-section2">
+      <div className="footer-section2 flex">
         <p>&copy; 2024 Subway System. All rights reserved.</p>
         <div>
           <img src="/imgs/facebook-icon.png" alt="" />
--- frontend/src/components/landingScreen/hero.jsx
+++ frontend/src/components/landingScreen/hero.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 // import { Link } from 'react-router-dom';
 import "../../styles/landingScreen/hero.css";
+import "../../styles/commons/colors.css";
+import "../../styles/commons/utilities.css";
 
 const Hero = () => {
   return (
     <div className="hero-section">
-      <header className="header">
+      <header className="header ">
         <img className="logo" src="/imgs/logo.png" alt="" />
-        <nav className="nav-home">
-          <ul className="nav-container">
+        <nav className="nav-home flex">
+          <ul className="nav-container flex ">
             <li>
               <p>Home</p>
             </li>
@@ -24,7 +26,7 @@ const Hero = () => {
           </ul>
         </nav>
       </header>
-        <section className="hero-section1">
+        <section className="hero-section1 flex center">
           <h2 className="hero-title">Explore the City's Heart</h2>
           <p className="hero-text">Where Every Journey is Part of the Destination</p>
         </section>
