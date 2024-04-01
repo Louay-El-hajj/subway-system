4acd376a087fdee7b6e5e3bb1462083d612d9c24
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-04-01 21:32:11 +0300

    .

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,42 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Route::post('create_station', [StationController::class, 'create_station']);
+// Route::post('update_station', [StationController::class, 'update_station']);
+// Route::post('display_station', [StationController::class, 'display_station']);
+// Route::post('delete_station', [StationController::class, 'delete_station']);
+
+Route::controller(StationController::class)->group(function () {
+    Route::get('get_station', "get_station");
+    Route::post('create_station', 'create_station');
+    Route::post('delete_station', 'delete_station');
+    Route::post('update_station', 'update_station');
+});
+
+Route::controller(UserController::class)->group((function () {
+    Route::get('get_user', 'get_user');
+    Route::post('edit_user', 'edit_user');
+    Route::post('upload_pic', 'upload_pic');
+}));
+
+Route::prefix('admin/')->group((function () {
+    Route::controller(AdminAuthController::class)->group((function () {
+        Route::post('login', 'login');
+        Route::post('logout', 'logout');
+        Route::post('refresh', 'refresh');
+    }));
+    Route::controller(AdminController::class)->group((function () {
+        Route::get('get_driver_registration_requests', 'getDriverRegisterRequests');
+        Route::get('get_all_drivers', 'getAllDrivers');
+        Route::get('get_all_passengers', 'getAllPassengers');
+        Route::post('approve', 'approve');
+        Route::post('deny', 'deny');
+        Route::post('get_user', 'get_user');
+        Route::post('edit_driver', 'edit_driver');
+        Route::post('delete_user', 'delete_user');
+        Route::post('get_user_orders', 'get_user_orders');
+        Route::post('edit_user', 'edit_user');
+        Route::post('driver_analytics', 'driver_analytics');
+        Route::post('passenger_analytics', 'passenger_analytics');
+    }));
+}));
