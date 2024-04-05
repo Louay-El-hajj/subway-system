02b77d605303d5f601345d14649cae505441df74
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-04-05 07:23:34 +0300

    .

--- backend/app/Http/Controllers/headQuartersController.php
+++ backend/app/Http/Controllers/headQuartersController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Branch;
-use App\Models\CoinRequest;
-use App\Models\User;
 
 class HeadquartersController extends Controller
 {
